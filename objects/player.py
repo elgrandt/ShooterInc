4070cfcbf36a209a0891656b750ac76f124404a6
newtonis <newtonis.penguin@gmail.com>
2016-03-02 23:58:26 -0300

    test

--- objects/player.py
+++ objects/player.py
@@ -1,11 +1,16 @@
 import OGL
 from macros import *
+from math import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 class Player(OGL.ComplexObject):
     def __init__(self):
         OGL.ComplexObject.__init__(self,0,0,0)
         self.speed = .3
         self.angle = 0
+
+
     def logic(self):
         if KEY(pygame.K_LEFT):
             self.x -= self.speed
@@ -16,6 +21,13 @@ class Player(OGL.ComplexObject):
         if KEY(pygame.K_DOWN):
             self.z += self.speed
         if KEY(pygame.K_a):
-            self.angle -= 1
+            self.angle -= 0.1
+            self.angle = (self.angle+360)%360
         if KEY(pygame.K_d):
-            self.angle += 1
+            self.angle += 0.1
+            self.angle = (self.angle+360) % 360
+
+        plus_a = sin(self.angle) * 20;
+        plus_b = cos(self.angle) * 20;
+        print plus_a , plus_b
+        gluLookAt( self.x , self.y , self.z , self.x - plus_a, self.y , self.z - plus_b , 0 , 1 , 0)
---
+++ shoot.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+### In this file thorically there will be the math to calculate if a shoot hits some cube or sphere.
+
+### First we'll try to analise 2d collsions
+
+def GetFirstShot(x , y , angle , pointList):
+	pass
