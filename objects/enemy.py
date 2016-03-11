473fd6a4bc54eea0eebb4f4c61d32dde80a7131c
newtonis <newtonis.penguin@gmail.com>
2016-03-11 20:37:54 -0300

    updated

--- objects/enemy.py
+++ objects/enemy.py
@@ -1,15 +1,17 @@
 import OGL
+import pywavefront
 
 class BasicEnemy(OGL.Cube):
     def __init__(self,x,y,z):
         OGL.Cube.__init__(self,3,8,2,x,y,z)
-        self.setColor(255,0,0)
-        self.setFilled(True)
-        #self.model = OGL.OBJ("Man.obj",False,10)
-        #self.width = self.model.width
+        self.model = pywavefront.Wavefront("Rock.obj","models/",10)
+        #self.model = OGL.OBJ("Man.obj",Fse,10)
+        #self.width = self.model.widthal
         #self.height = self.model.height
         #self.depth = self.model.depth
-    #def blit(self):
+
+    def blit(self):
+        self.model.draw()
         #if self.model.loaded:
         #    self.model.pos[1] = 0
         #self.model.blit()
