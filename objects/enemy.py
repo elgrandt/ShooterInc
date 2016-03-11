7898492f87ad69e121e2562ec2e91b6817193576
newtonis <newtonis.penguin@gmail.com>
2016-03-11 15:45:32 -0300

    Some changes

--- objects/enemy.py
+++ objects/enemy.py
@@ -3,11 +3,13 @@ import OGL
 class BasicEnemy(OGL.Cube):
     def __init__(self,x,y,z):
         OGL.Cube.__init__(self,3,8,2,x,y,z)
-        self.model = OGL.OBJ("Man.obj",False,10)
-        self.width = self.model.width
-        self.height = self.model.height
-        self.depth = self.model.depth
-    def blit(self):
-        if self.model.loaded:
-            self.model.pos[1] = 0
-        self.model.blit()
+        self.setColor(255,0,0)
+        self.setFilled(True)
+        #self.model = OGL.OBJ("Man.obj",False,10)
+        #self.width = self.model.width
+        #self.height = self.model.height
+        #self.depth = self.model.depth
+    #def blit(self):
+        #if self.model.loaded:
+        #    self.model.pos[1] = 0
+        #self.model.blit()
