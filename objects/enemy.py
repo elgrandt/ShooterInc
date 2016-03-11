b104f060b2f286b412e7c14c8cdda977549b966c
newtonis <newtonis.penguin@gmail.com>
2016-03-11 15:27:53 -0300

    Some changes

--- objects/enemy.py
+++ objects/enemy.py
@@ -3,7 +3,7 @@ import OGL
 class BasicEnemy(OGL.Cube):
     def __init__(self,x,y,z):
         OGL.Cube.__init__(self,3,8,2,x,y,z)
-        self.model = OGL.OBJ("Man.obj",False,50)
+        self.model = OGL.OBJ("Man.obj",False,10)
         self.width = self.model.width
         self.height = self.model.height
         self.depth = self.model.depth
